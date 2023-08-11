c6f023a407e81f6bcdc9785893486961ebde0946
cbailey73 <123669370+cbailey73@users.noreply.github.com>
2023-08-11 11:20:40 -0500

    starter code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Fetch posts for the homepage
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postsData = await Posts.findAll({
+      order: [['updatedOn', 'DESC']],
+    });
+
+    const posts = postsData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
